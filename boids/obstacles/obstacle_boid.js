5273a4bcc1b47f2dbc0d0d75d6e7267d8638dfda
ShadowCrafter011 <lkoe@bluewin.ch>
2025-01-17 10:08:18 +0100

    Adapted obstacle boid visual range

--- boids/obstacles/obstacle_boid.js
+++ boids/obstacles/obstacle_boid.js
@@ -1,6 +1,7 @@
 class ObstacleBoid extends Boid {
     constructor(...args) {
         super(...args);
+        this.visual_range = 100;
         this.obstacle_range = 100;
         this.obstacle_turn_factor = 0.5;
     }
@@ -10,7 +11,7 @@ class ObstacleBoid extends Boid {
             if (!CircleObstacle.prototype.isPrototypeOf(obstacle)) continue;
             if (!obstacle.boid_in_range(this)) continue;
             let angle = this.vel.angleBetween(obstacle.pos.copy().sub(this.pos));
-            angle = Math.max(1e-6, Math.abs(angle));
+            angle = Math.max(1e-1, Math.abs(angle));
             let obstacle_boid = this.pos.copy().sub(obstacle.pos).normalize();
             obstacle_boid.mult(1 / angle * this.obstacle_turn_factor);
             this.vel.add(obstacle_boid);
