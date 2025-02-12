8877307c7a842a906389388fdd4b9636bf7ceba4
ShadowCrafter011 <lkoe@bluewin.ch>
2025-02-12 11:58:38 +0100

    Start on index page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const elements = document.getElementsByTagName("iframe");
-const remSize = parseFloat(getComputedStyle(document.documentElement).fontSize); // 1rem in pixels
-const parentWidth = document.getElementById("boids").offsetWidth;
+document.getElementById("view-page-button").addEventListener("click", () => {
+    document.getElementsByTagName("phone-disclaimer")[0].style.display = "none";
+    document.getElementsByTagName("main")[0].style.display = "block";
+});
 
-for (let element of elements) {
-    let scaleValue = 0.25 - (remSize / parentWidth);
-    element.style.transform = `scale(${scaleValue})`;
-}
+// const elements = document.getElementsByTagName("iframe");
+// const remSize = parseFloat(getComputedStyle(document.documentElement).fontSize); // 1rem in pixels
+// const parentWidth = document.getElementById("boids").offsetWidth;
+
+// for (let element of elements) {
+//     let scaleValue = 0.25 - (remSize / parentWidth);
+//     element.style.transform = `scale(${scaleValue})`;
+// }
