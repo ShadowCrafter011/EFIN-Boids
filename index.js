7702c17e17adf9622242d858b27355b2728842ca
ShadowCrafter011 <lkoe@bluewin.ch>
2025-02-12 19:34:59 +0100

    Removed commented out code

--- index.js
+++ index.js
@@ -2,12 +2,3 @@ document.getElementById("view-page-button").addEventListener("click", () => {
     document.getElementsByTagName("phone-disclaimer")[0].style.display = "none";
     document.getElementsByTagName("main")[0].style.display = "block";
 });
-
-// const elements = document.getElementsByTagName("iframe");
-// const remSize = parseFloat(getComputedStyle(document.documentElement).fontSize); // 1rem in pixels
-// const parentWidth = document.getElementById("boids").offsetWidth;
-
-// for (let element of elements) {
-//     let scaleValue = 0.25 - (remSize / parentWidth);
-//     element.style.transform = `scale(${scaleValue})`;
-// }
